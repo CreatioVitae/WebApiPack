5f021a26b9bf0806a0806031880ee12fa517410a
h.chiba <h.chiba201103@gmail.com>
2019-11-26 15:32:58 +0900

    Edit CreateHttpGetResult(Object => Nullable Object);

--- src/WebApiPack/Controllers/ControllerBaseExtensions.cs
+++ src/WebApiPack/Controllers/ControllerBaseExtensions.cs
@@ -10,10 +10,14 @@ namespace WebApiPack.Controllers {
             return controllerBase.StatusCode(StatusCodes.Status405MethodNotAllowed, new ErrorResult($"リソース[{controllerBase.HttpContext.GetPathPlusQueryStrings()}]では、メソッド[{controllerBase.HttpContext.GetHttpMethod()}]は許可されていません。"));
         }
 
-        public static ActionResult CreateHttpGetResult(this ControllerBase controllerBase, object apiResult) {
+        public static ActionResult CreateHttpGetResult(this ControllerBase controllerBase, object? apiResult) {
             return apiResult is IEnumerable<object> e
-                ? e.Any() ? (ObjectResult) controllerBase.Ok(apiResult) : controllerBase.NotFound(new ErrorResult($"リソース[{controllerBase.HttpContext.GetPathPlusQueryStrings()}]が見つかりません"))
-                : apiResult != default ? (ObjectResult) controllerBase.Ok(apiResult) : controllerBase.NotFound(new ErrorResult($"リソース[{controllerBase.HttpContext.GetPathPlusQueryStrings()}]が見つかりません"));
+                ? e.Any() 
+                    ? (ObjectResult) controllerBase.Ok(apiResult) 
+                    : controllerBase.NotFound(new ErrorResult($"リソース[{controllerBase.HttpContext.GetPathPlusQueryStrings()}]が見つかりません"))
+                : (apiResult != default)
+                    ? (ObjectResult) controllerBase.Ok(apiResult) 
+                    : controllerBase.NotFound(new ErrorResult($"リソース[{controllerBase.HttpContext.GetPathPlusQueryStrings()}]が見つかりません"));
         }
     }
 }
