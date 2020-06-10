684f38d3c4af8c8fbd8625f9c983d0a76e27a161
CreatioVitae <h.chiba201103@gmail.com>
2020-06-10 22:26:13 +0900

    Add GlobalFilter Injection

--- src/WebApiPack/ConfigureServices/ServiceCollectionExtensions.cs
+++ src/WebApiPack/ConfigureServices/ServiceCollectionExtensions.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using Utf8Json.Resolvers;
 using WebApiPack.ConstantValues;
 
 namespace Microsoft.Extensions.DependencyInjection {
     public static class ServiceCollectionExtensions {
-        public static IServiceCollection CreateDefaultBuilder(this IServiceCollection serviceDescriptors) {
+        public static IServiceCollection CreateDefaultBuilder(this IServiceCollection serviceDescriptors, IEnumerable<Type>? filterTypesForGlobalEntry = null) {
             serviceDescriptors
                 .AddHttpContextAccessor()
                 .AddControllers();
@@ -15,6 +17,12 @@ namespace Microsoft.Extensions.DependencyInjection {
                     option.OutputFormatters.Clear();
                     option.OutputFormatters.Add(new Utf8Json.AspNetCoreMvcFormatter.JsonOutputFormatter(StandardResolver.ExcludeNullCamelCase));
                     option.Filters.Add(new ProducesAttribute("text/plain", "application/json", "text/json"));
+
+                    if (filterTypesForGlobalEntry is IEnumerable<Type>) {
+                        foreach (var filterTypeForGlobalEntry in filterTypesForGlobalEntry) {
+                            option.Filters.Add(filterTypeForGlobalEntry);
+                        }
+                    }
                 })
                 .AddJsonOptions(option => option.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()))
                 .AddCors(option => option.AddPolicy(CorsConstantValues.PolicyName, builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
