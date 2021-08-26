ae79d906e61b06bc5567ceeb2faa6456ec31f7f1
CreatioVitae <h.chiba201103@gmail.com>
2021-08-26 21:31:14 +0900

    add : add basic auth swagger settings...

---
+++ src/Swagger.Extensions.DependencyInjection/Configurations/SwaggerGenSettings.cs
@@ -0,0 +1,25 @@
+namespace Swagger.Extensions.DependencyInjection.Configurations {
+    public class SwaggerGenSettings {
+        public AuthenticationSettings AuthenticationSettings { get; init; }
+
+        public SwaggerDocSettings SwaggerDocSettings { get; init; }
+    }
+
+    public class AuthenticationSettings {
+        public bool AuthenticationIsEnable { get; init; }
+
+        public string AuthorizationType { get; init; }
+    }
+
+    public class SwaggerDocSettings {
+        public string DocName { get; init; }
+
+        public DocInfo Info { get; init; }
+    }
+
+    public class DocInfo {
+        public string Title { get; init; }
+
+        public string Version { get; init; }
+    }
+}
--- src/Swagger.Extensions.DependencyInjection/ConfigureServices/ServiceCollectionExtensions.cs
+++ src/Swagger.Extensions.DependencyInjection/ConfigureServices/ServiceCollectionExtensions.cs
@@ -1,24 +1,71 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.OpenApi.Models;
 using Swagger.Extensions.DependencyInjection.Configurations;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
 
 namespace Microsoft.Extensions.DependencyInjection {
     public static class ServiceCollectionExtensions {
-        public static IServiceCollection AddDefaultSwaggerService(this IServiceCollection serviceDescriptors, SwaggerDocSettings swaggerDocSettings) =>
+        public static IServiceCollection AddDefaultSwaggerService(this IServiceCollection serviceDescriptors, SwaggerGenSettings swaggerGenSettings) =>
             serviceDescriptors.AddSwaggerGen(c => {
-                c.SwaggerDoc(swaggerDocSettings.DocName, new OpenApiInfo { Title = swaggerDocSettings.Info.Title, Version = swaggerDocSettings.Info.Version });
-                c.CustomOperationIds(apiDesc => apiDesc.TryGetMethodInfo(out var methodInfo) ? methodInfo.Name.RemoveFromEnd("Async") : null);
+                c.DefaultSwaggerDoc(swaggerGenSettings.SwaggerDocSettings);
+                c.AddDefaultAuthentication(swaggerGenSettings);
+                c.DefaultCustomOperationIds();
                 c.DocumentFilter<AdditionalParametersDocumentFilter>();
             });
 
-        public static IServiceCollection AddDefaultSwaggerService<TFilter>(this IServiceCollection serviceDescriptors, SwaggerDocSettings swaggerDocSettings) where TFilter : IOperationFilter =>
+        public static IServiceCollection AddDefaultSwaggerService<TFilter>(this IServiceCollection serviceDescriptors, SwaggerGenSettings swaggerGenSettings) where TFilter : IOperationFilter =>
             serviceDescriptors.AddSwaggerGen(c => {
-                c.SwaggerDoc(swaggerDocSettings.DocName, new OpenApiInfo { Title = swaggerDocSettings.Info.Title, Version = swaggerDocSettings.Info.Version });
-                c.CustomOperationIds(apiDesc => apiDesc.TryGetMethodInfo(out var methodInfo) ? methodInfo.Name.RemoveFromEnd("Async") : null);
+                c.DefaultSwaggerDoc(swaggerGenSettings.SwaggerDocSettings);
+                c.AddDefaultAuthentication(swaggerGenSettings);
+                c.DefaultCustomOperationIds();
                 c.DocumentFilter<AdditionalParametersDocumentFilter>();
 
                 c.OperationFilter<TFilter>();
             });
+
+        internal static void AddDefaultAuthentication(this SwaggerGenOptions c, SwaggerGenSettings swaggerGenSettings) {
+            if (swaggerGenSettings.AuthenticationSettings.AuthenticationIsEnable is false) {
+                return;
+            }
+
+            switch (swaggerGenSettings.AuthenticationSettings.AuthorizationType) {
+                case AuthorizationType.Basic:
+                    c.AddBasicAuthentication(swaggerGenSettings);
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        internal static void AddBasicAuthentication(this SwaggerGenOptions c, SwaggerGenSettings swaggerGenSettings) {
+            c.AddSecurityDefinition(AuthorizationType.Basic, new OpenApiSecurityScheme {
+                Name = HttpHeaderConsts.AuthorizationHeaderKey,
+                Type = SecuritySchemeType.Http,
+                Scheme = swaggerGenSettings.AuthenticationSettings.AuthorizationType,
+                In = ParameterLocation.Header,
+                Description = "Basic Authorization header using the Bearer scheme."
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                {
+                    new OpenApiSecurityScheme {
+                        Reference = new OpenApiReference {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = AuthorizationType.Basic
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+        }
+
+        internal static void DefaultSwaggerDoc(this SwaggerGenOptions c, SwaggerDocSettings swaggerDocSettings) =>
+            c.SwaggerDoc(swaggerDocSettings.DocName, new OpenApiInfo { Title = swaggerDocSettings.Info.Title, Version = swaggerDocSettings.Info.Version });
+
+        internal static void DefaultCustomOperationIds(this SwaggerGenOptions c) =>
+            c.CustomOperationIds(apiDesc => apiDesc.TryGetMethodInfo(out var methodInfo) ? methodInfo.Name.RemoveFromEnd("Async") : null);
 
         internal static string RemoveFromEnd(this string target, string suffix) =>
             target.EndsWith(suffix)
