42325f7726398e9cac458f60cac605d7c98af7b0
CreatioVitae <h.chiba201103@gmail.com>
2022-02-11 23:09:43 +0900

    update nuget package...

--- src/Swagger.Extensions.DependencyInjection/Configurations/SwaggerSettings.cs
+++ src/Swagger.Extensions.DependencyInjection/Configurations/SwaggerSettings.cs
@@ -1,18 +1,18 @@
 namespace Swagger.Extensions.DependencyInjection.Configurations;
 
 public class SwaggerSettings {
-    public RouteTemplateSettings RouteTemplateSettings { get; set; }
+    public RouteTemplateSettings RouteTemplateSettings { get; set; } = null!;
 
-    public OpenApiServerPathBaseSettings OpenApiServerPathBaseSettings { get; set; }
+    public OpenApiServerPathBaseSettings OpenApiServerPathBaseSettings { get; set; } = null!;
 }
 
 public class OpenApiServerPathBaseSettings {
     public bool IsOverridable { get; set; }
-    public string PathBase { get; set; }
+    public string PathBase { get; set; } = null!;
     public bool ForceHttps { get; set; }
 }
 
 public class RouteTemplateSettings {
     public bool IsOverridable { get; set; }
-    public string RouteTemplatePrefix { get; set; }
+    public string RouteTemplatePrefix { get; set; } = null!;
 }
--- src/Swagger.Extensions.DependencyInjection/Configurations/SwaggerUiSettings.cs
+++ src/Swagger.Extensions.DependencyInjection/Configurations/SwaggerUiSettings.cs
@@ -1,12 +1,12 @@
 namespace Swagger.Extensions.DependencyInjection.Configurations;
 
 public class SwaggerUiSettings {
-    public string Url { get; set; }
-    public string Name { get; set; }
-    public RoutePrefixSettings RoutePrefixSettings { get; set; }
+    public string Url { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public RoutePrefixSettings RoutePrefixSettings { get; set; } = null!;
 }
 
 public class RoutePrefixSettings {
     public bool IsOverridable { get; set; }
-    public string RoutePrefix { get; set; }
+    public string RoutePrefix { get; set; } = null!;
 }
