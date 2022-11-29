d0f221a04dcf6549322b3e9a2227ca7021b73417
CreatioVitae <h.chiba201103@gmail.com>
2022-11-29 14:26:21 +0900

    remove utf8json.

--- src/WebApiPack/ConfigureServices/ServiceCollectionExtensions.cs
+++ src/WebApiPack/ConfigureServices/ServiceCollectionExtensions.cs
@@ -1,34 +1,29 @@
-using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
-using Utf8Json.Resolvers;
 using WebApiPack.ConstantValues;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions {
-    public static IServiceCollection CreateDefaultBuilder(this IServiceCollection serviceDescriptors, IEnumerable<Type>? filterTypesForGlobalEntry = null, bool useUtf8JsonAsOutputFormatter = true) {
+    public static IServiceCollection CreateDefaultBuilder(this IServiceCollection serviceDescriptors, IEnumerable<Type>? filterTypesForGlobalEntry = null) {
         serviceDescriptors
             .AddHttpContextAccessor()
             .AddControllers();
 
         serviceDescriptors
             .AddMvcCore(option => {
-                if (useUtf8JsonAsOutputFormatter) {
-                    option.OutputFormatters.Clear();
-                    option.OutputFormatters.Add(new Utf8Json.AspNetCoreMvcFormatter.JsonOutputFormatter(StandardResolver.ExcludeNullCamelCase));
-                    option.Filters.Add(new ProducesAttribute("text/plain", "application/json", "text/json"));
-                }
-
                 if (filterTypesForGlobalEntry is not null) {
                     foreach (var filterTypeForGlobalEntry in filterTypesForGlobalEntry) {
                         option.Filters.Add(filterTypeForGlobalEntry);
                     }
                 }
             })
-            .AddJsonOptions(option => option.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()))
+            .AddJsonOptions(option => {
+                option.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                option.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            })
             .AddCors(option => option.AddPolicy(CorsConst.PolicyName, builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 
         return serviceDescriptors;
