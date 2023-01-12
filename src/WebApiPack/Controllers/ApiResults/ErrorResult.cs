f569a134b85e2e0c36a224c3a41eee63713bc492
CreatioVitae <h.chiba201103@gmail.com>
2023-01-12 09:14:20 +0900

    repair ErrorResult.

--- src/WebApiPack/Controllers/ApiResults/ErrorResult.cs
+++ src/WebApiPack/Controllers/ApiResults/ErrorResult.cs
@@ -8,9 +8,17 @@ public class ErrorResult {
 
     public ErrorResult(string? message, Exception? exception = default) {
         Message = message;
-        Exception = exception;
+
+        Exception = ExceptionInfo.Create(exception);
     }
 
     public string? Message { get; }
-    public Exception? Exception { get; }
+    public ExceptionInfo? Exception { get; }
 }
+
+public record ExceptionInfo(string Value) {
+    public static ExceptionInfo? Create(Exception? e) =>
+        e is null
+            ? default
+            : new ExceptionInfo(e.ToString());
+} 
